1bf044951c0064e883d78f20eb8109e8170990e3
Soy-Mauricio <maurosana6@gmail.com>
2024-04-05 21:09:26 -0500

    MS conexión Axios

--- WebPage/frontend/src/App.js
+++ WebPage/frontend/src/App.js
@@ -1,42 +1,17 @@
-import axios from 'axios';
 import React from 'react';
+import Axios from '../src/Componentes/Axios/axios';
+import './App.css'
 
-class App extends React.Component {
-  state = { details: [], }
+function App() {
+  return (
+    <div className="App">
 
-  componentDidMount() {
+    <Axios/>
 
-    let data;
-    axios.get('http://127.0.0.1:8000/product/')
-      .then(res => {
-        data = res.data;
-        this.setState({
-          details: data
-        });
-      })
-      .catch(err => {})
+    </div>
+  );
 
-  }
 
-  render() {
-    return(
-      <div>
-        <header>Test API</header>
-        <hr></hr>
-        {this.state.details.map((output, id) => (
-          <div key={id}>
-            <div>
-              <h3>{output.description}</h3>
-              {output.product_id}<br></br>
-              {output.amount}<br></br>
-              {output.unit_price}
-            </div>
-          </div>
-        ))}
-      </div>
-    )
-  }  
 }
-
 
 export default App;
---
+++ WebPage/frontend/src/Componentes/Axios/axios.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Axios.css";
+
+const Axios = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/product/").then((response) => {
+        setData(response.data);
+      })
+
+  }, []);
+
+  return (
+
+    <div className="contenedor-mayor">
+      <h1 className="titulo">Datos desde Django REST Framework</h1>
+      <div className="contenedor-menor">
+        {data.map((item) => (
+          <ul key={item.id} className="contenedor-lista">
+
+            <li>ID Producto: {item.product_id}</li>
+            <li>Categoria: {item.category}</li>
+            <li>Descripción: {item.description}</li>
+            <li>Cantidad: {item.amount}</li>
+            <li>Precio unidad: {item.unit_price}</li>
+
+          </ul>
+        ))}
+      </div>
+    </div>
+
+  );
+};
+
+export default Axios;
+
