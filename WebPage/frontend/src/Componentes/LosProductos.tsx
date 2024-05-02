6e92c1573d5acf66189cfc41701d3c1a180ce267
Soy-Mauricio <maurosana6@gmail.com>
2024-05-02 18:16:50 -0500

    Creación de botones de paginación

--- WebPage/frontend/src/Componentes/LosProductos.tsx
+++ WebPage/frontend/src/Componentes/LosProductos.tsx
@@ -1,36 +1,40 @@
-import { useEffect, useState } from 'react';
-import { ListaProductos } from './ListaProductos';
-import { DatosProductos } from '../Api/DatosDelProducto';
-import { Paginacion } from '../Componentes/Paginacion';
-import { Footer } from '../Componentes/Footer'
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import Footer from "./Footer";
 
 import imgCafeUno from '../Img/CafeUno.jpg';
 import imgCafeDos from '../Img/CafeDos.jpg';
 import imgCafeTres from '../Img/CafeTres.jpg';
 
-const LosProductos = () => {
-
-    interface Product {
-        product_id: string;
-        url: string;
-        category_data: {
-            category: string;
-        };
-        description: string;
-        amount: number;
-        unit_price: string;
-    }
-
-
-    const [products, setProducts] = useState<Product[]>([]);
+    const LosProductos = () => {
+    const [products, setProducts] = useState([]);
+    const [nextPage, setNextPage] = useState("");
+    const [prevPage, setPrevPage] = useState("");
 
     useEffect(() => {
-        async function cargarDatos() {
-            const res = await DatosProductos();
-            setProducts(res);
-        }
-        cargarDatos();
+        fetchProducts("http://127.0.0.1:8000/API/products/");
     }, []);
+
+    const fetchProducts = async (url) => {
+        try {
+        const response = await axios.get(url);
+        setProducts(response.data.results);
+        setNextPage(response.data.next);
+        setPrevPage(response.data.previous);
+        } catch (error) {
+        console.error("Error fetching products:", error);
+        }
+    };
+
+    const handleNextPage = () => {
+        fetchProducts(nextPage);
+    };
+
+    const handlePrevPage = () => {
+        fetchProducts(prevPage);
+    };
+    
 
     const imagenesProductos = {
         "00101": imgCafeUno,
@@ -39,36 +43,65 @@ const LosProductos = () => {
         // Añade más imágenes según sea necesario
     };
 
+
     return (
 
         <>
-        <section className="my-20 lg:flex lg:justify-center md:mt-20 lg:mt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
-            
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 lg:w-[85%]">
-                {products.map((product) => (
-                    <ListaProductos
-                        key={product.product_id}
-                        product={product}
-                        img={imagenesProductos[product.product_id]}
-                    />
-                ))}
 
+        <section>
+
+            <div className="my-20 lg:flex lg:justify-center md:mt-20 lg:mt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 lg:w-[85%]">
+                    {products.map((product) => (
+                        <li className="list-none" key={product.product_id}>
+                            <div className="flex justify-center h-full sm:w-full">
+                                <div className="w-[325px] lg:w-[290px] md:w-[60%] h-[auto] mb-10 cursor-pointer border border-white hover:border-solid hover:border hover:border-black transition duration-700 ease-in-out">
+                                    <div className="w-[full] h-[250px] relative">
+                                        <img className="absolute inset-0 w-full h-full object-cover" src={imagenesProductos[product.product_id]} alt="" />
+                                    </div>
+                                    <div className="w-[auto] h-[auto] mt-2 p-2">
+                                        <p className="text-[18px] w-[100%] h-[100%] mb-5">{product.description}</p>
+                                        <p className="text-[18px] w-[100%] h-[100%] mb-5">{product.unit_price}</p>
+                                        <p className="text-[18px] w-[100%] h-[100%] mb-5">{product.amount}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </div>
+
+            </div>
+
+            <div className="my-20 lg:flex lg:justify-center md:mt-20 lg:mt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
+
+                <div className="w-[25%] flex justify-between">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={handlePrevPage}
+                        disabled={!prevPage}>
+                        Anterior
+                    </button>
+                    
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={handleNextPage}
+                        disabled={!nextPage}>
+                        Siguiente
+                    </button>
+                </div>
+                
             </div>
 
         </section>
 
-        <section className="my-20 lg:flex lg:justify-center md:mt-20 lg:mt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
-
-            <Paginacion />
-
-        </section>
-
         <footer>
-            <Footer />
+            <Footer/>
         </footer>
 
         </>
+
     );
-}
+};
 
 export default LosProductos;
