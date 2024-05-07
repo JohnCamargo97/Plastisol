940a769283fa73b485daf744344dce7585692946
Soy-Mauricio <maurosana6@gmail.com>
2024-05-06 21:03:39 -0500

    Mejora de botones

--- WebPage/frontend/src/Componentes/Navbar.tsx
+++ WebPage/frontend/src/Componentes/Navbar.tsx
@@ -1,6 +1,7 @@
 import cerrarMenu from '../Img/Icono-cruz.png';
 import menuBurger from '../Img/Icono-barras-escalonadas.png';
 import anguloAbajo from '../Img/Icono-angulo-hacia-abajo.png';
+import siguiente from '../Img/Icono-flecha.png';
 
 /*import correo from '../Img/Icono-sobre.png';*/
 import logo from '../Img/StarbucksLogo.png';
@@ -61,7 +62,7 @@ export function Navbar() {
       <ul className={`md:flex md:items-center md:space-x-10  md:ml-auto mt-2 bg-white md:relative md:z-auto z-10 w-full md:w-auto md:pl-0 transition-all duration-500 ease-in  ${isOpen ? 'block' : 'hidden md:block'}`}>
         <li className="font-semibold my-2 md:my-0 relative mt-4">
           <a className="flex items-center md:border-b-2 border-white transition ease-in duration-300 md:hover:border-solid md:hover:border-b-2 hover:border-slate-800" href="/" onClick={(e) => {e.preventDefault(); toggleContacto();}}>Contactanos <img className="h-[25px] w-[25px] mt-1" src={anguloAbajo} alt="" /></a>
-          <ul className={`mt-2 ${isContactoOpen ? 'block' : 'hidden'} cursor-pointer md:absolute md:left-0 md:top-full md:w-[auto] md:h-[auto] md:bg-white md:shadow-md md:z-20 md:border-solid md:border-2 rounded md:border-gray-300 md:py-2`}>
+          <ul className={`mt-2 ${isContactoOpen ? 'block' : 'hidden'} cursor-pointer md:absolute md:left-0 md:top-full md:w-[auto] md:h-[auto] md:bg-white md:shadow-md md:z-20 md:border-solid md:border-2 md:border-gray-900 md:py-2`}>
             {Contacto.map((contacto, index) => (
               <li key={index} className="hover:bg-slate-200 h-10 flex items-center">
                 <a className="font-medium md:text-[16px] p-2" href={contacto.link} onClick={closeMenus}>
@@ -74,7 +75,7 @@ export function Navbar() {
 
         <li className="font-semibold my-2 md:my-0 relative mt-4">
           <a className="flex items-center md:border-b-2 border-white transition ease-in duration-300 md:hover:border-solid md:hover:border-b-2 hover:border-slate-800" href="/" onClick={(e) => {e.preventDefault(); toggleProductosMenu();}}>Productos <img className="h-[25px] w-[25px] mt-1" src={anguloAbajo} alt="" /></a>
-          <ul className={`mt-2 ${isProductosOpen ? 'block' : 'hidden'} cursor-pointer md:absolute md:left-0 md:top-full md:w-[120px] md:h-[auto] md:bg-white md:shadow-md md:z-20 md:border-solid md:border-2 rounded md:border-gray-300 md:py-2`}>
+          <ul className={`mt-2 ${isProductosOpen ? 'block' : 'hidden'} cursor-pointer md:absolute md:left-0 md:top-full md:w-[120px] md:h-[auto] md:bg-white md:shadow-md md:z-20 md:border-solid md:border-2 md:border-gray-900 md:py-2`}>
             {Productos.map((producto, index) => (
               <li key={index} className="hover:bg-slate-200 h-10 flex items-center">
                 <a className="font-medium md:text-[16px] p-2" href={producto.link} onClick={closeMenus}>
@@ -88,8 +89,11 @@ export function Navbar() {
         <div className="border-b border-slate-800 md:hidden"></div>
           
         <div className="font-semibold my-4 md:my-0 border border-black items-start">
-          <div className=" mb-2.5 mr-1 ">
-            <a className="bg-slate-950 hover:bg-slate-900 text-white py-2 px-10 -ml-2" href="/">Cotización</a>
+          <div className=" mb-1 mr-1">
+            <a className="bg-slate-950 hover:bg-slate-900 text-white py-2 px-8 -ml-2 -mt-2 flex items-center" href="/">
+              <span>Cotización</span>
+              <span className='ml-1'><img className='w-[25px] h-[25px]' src={siguiente} alt="" /></span>
+            </a>
           </div>
         </div>
 
